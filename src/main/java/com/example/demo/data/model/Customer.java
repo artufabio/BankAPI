306e37ca6a9d7e149f47359756b26f8927410fac
Fabio Artuso <fabioartuso88@gmail.com>
2021-11-29 16:56:10 +0000

    Created Customer class with all the required properties

--- src/main/java/com/example/demo/data/model/Customer.java
+++ src/main/java/com/example/demo/data/model/Customer.java
@@ -1,5 +1,154 @@
 package com.example.demo.data.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private LocalDate dob;
+	private String nationality;
+	private String address;
+	private String email;
+	private Integer account;
+	private Integer startingBalance;
+	private String username;
+	private String password;
+	
+	public Customer() {}
+
+	
+
+	public Customer(
+			Integer id, 
+			String firstName, 
+			String lastName, 
+			LocalDate dob, 
+			String nationality, 
+			String address,
+			String email, 
+			Integer account, 
+			Integer startingBalance, 
+			String username, 
+			String password) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.nationality = nationality;
+		this.address = address;
+		this.email = email;
+		this.account = account;
+		this.startingBalance = startingBalance;
+		this.username = username;
+		this.password = password;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getAccount() {
+		return account;
+	}
+
+	public void setAccount(Integer account) {
+		this.account = account;
+	}
+
+	public Integer getStartingBalance() {
+		return startingBalance;
+	}
+
+	public void setStartingBalance(Integer startingBalance) {
+		this.startingBalance = startingBalance;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", dob=" + dob
+				+ ", nationality=" + nationality + ", address=" + address + ", email=" + email + ", account=" + account
+				+ ", startingBalance=" + startingBalance + ", username=" + username + ", password=" + password + "]";
+	}
+	
+	
+	
+	
 }
