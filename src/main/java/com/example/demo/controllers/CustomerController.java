acaa085a59ce16437c0f36dff9ed19389d54a664
Fabio Artuso <fabioartuso88@gmail.com>
2021-11-30 10:59:29 +0000

    Created Controller Component

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,5 +1,17 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.CustomerService;
+
+@RestController
+@RequestMapping(path="/customer")
 public class CustomerController {
 
+	private CustomerService service;
+	
+	public CustomerController(CustomerService service) {
+		this.service = service;
+	}
 }
