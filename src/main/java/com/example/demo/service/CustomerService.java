2042dba2336b78b9dcf7edcc5366f1e70826436a
Fabio Artuso <fabioartuso88@gmail.com>
2021-11-30 10:49:17 +0000

    Created Service component

--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -1,5 +1,15 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repo.CustomerRepo;
+
+@Service
 public class CustomerService {
 
+	private CustomerRepo repo;
+	
+	public CustomerService(CustomerRepo repo) {
+		this.repo = repo;
+	}
 }
