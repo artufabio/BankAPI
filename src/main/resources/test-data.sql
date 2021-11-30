a4119e17389ed2721fc3b3b35258b10670cf10e7
Fabio Artuso <fabioartuso88@gmail.com>
2021-11-30 14:10:46 +0000

    Created test-schema and test-data SQL files for testing

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,51 @@
+insert into customer 
+(
+	account,
+	address,
+	dob,
+	email,
+	first_name,
+	last_name,
+	nationality,
+	password,
+	starting_balance,
+	username
+) values 
+(
+	123456,
+	'Regent Street 103',
+	'1948-12-04',
+	'email@gmail.com'
+	'Elvis',
+	'Presley',
+	'American',
+	'secret123',
+	120000,
+	'elvis25'
+);
+
+insert into customer 
+(
+	account,
+	address,
+	dob,
+	email,
+	first_name,
+	last_name,
+	nationality,
+	password,
+	starting_balance,
+	username
+) values 
+(
+	654321,
+	'Regent Street 301',
+	'1984-01-08',
+	'email2@gmail.com'
+	'Freddy',
+	'Mercury',
+	'British',
+	'secret456',
+	1200,
+	'freddy25'
+);
---
+++ src/test/java/com/example/demo/controllers_test/CustomerControllerIntegrationTest.java
@@ -0,0 +1,5 @@
+package com.example.demo.controllers_test;
+
+public class CustomerControllerIntegrationTest {
+
+}
