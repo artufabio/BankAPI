21d90b5d76db8e7d226db1b7601f9f90e5e03f2b
Fabio Artuso <fabioartuso88@gmail.com>
2021-11-30 17:08:40 +0000

    Retrieved all customers from MySQL db. Displayed the result through localhost route

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +27,10 @@ public class CustomerController {
 	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
 		ResponseEntity<Customer> response = new ResponseEntity<Customer>(this.service.createCustomer(customer), HttpStatus.CREATED);
 		return response;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Customer>> getAllCustomers(){
+		return new ResponseEntity<List<Customer>>(this.service.getAllCustomers(), HttpStatus.OK);
 	}
 }
--- src/main/resources/test-schema.sql
+++ src/main/resources/test-schema.sql
@@ -4,7 +4,7 @@ create table customer
 	id integer not null auto_increment, 
 	account integer, 
 	address varchar(255), 
-	dob date, 
+	dob varchar(15), 
 	email varchar(255), 
 	first_name varchar(255), 
 	last_name varchar(255), 
