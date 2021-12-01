e4e385b6d5d2aaa47b5ead84411ae8a493ba06f4
Fabio Artuso <fabioartuso88@gmail.com>
2021-12-01 15:33:06 +0000

    Added updateCustomer method to the controller. Method successfully tested. Customer updated in the database through postman

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,4 +42,10 @@ public class CustomerController {
 		this.service.deleteCustomer(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
+	
+	@PutMapping("/update/{id}")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer, @PathVariable Integer id) {
+        ResponseEntity<Customer> response = new ResponseEntity<Customer>(this.service.updateCustomer(customer, id), HttpStatus.ACCEPTED); // 202
+        return response;
+    }
 }
