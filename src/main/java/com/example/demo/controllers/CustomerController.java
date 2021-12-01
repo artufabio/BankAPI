c3b18444bc1ad4e952b6fcba8474d6caa5289824
Fabio Artuso <fabioartuso88@gmail.com>
2021-12-01 15:03:29 +0000

    Added deleted files after reverting merged pull request

---
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.data.model.Customer;
+import com.example.demo.service.CustomerService;
+
+@RestController
+@RequestMapping(path="/customer")
+public class CustomerController {
+
+	private CustomerService service;
+	
+	public CustomerController(CustomerService service) {
+		this.service = service;
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+		ResponseEntity<Customer> response = new ResponseEntity<Customer>(this.service.createCustomer(customer), HttpStatus.CREATED);
+		return response;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Customer>> getAllCustomers(){
+		return new ResponseEntity<List<Customer>>(this.service.getAllCustomers(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<HttpStatus> deleteCustomer(@PathVariable Integer id) {
+		this.service.deleteCustomer(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
