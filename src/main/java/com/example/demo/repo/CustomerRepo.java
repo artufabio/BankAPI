669c4db8c40d5172df0053b66088f1a9f1c2cc06
Fabio Artuso <fabioartuso88@gmail.com>
2021-12-01 16:48:28 +0000

    Added custom query to the Repository

--- src/main/java/com/example/demo/repo/CustomerRepo.java
+++ src/main/java/com/example/demo/repo/CustomerRepo.java
@@ -1,6 +1,9 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.data.model.Customer;
@@ -8,4 +11,8 @@ import com.example.demo.data.model.Customer;
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Integer> {
 
+	
+//	Custom query 
+	@Query("select c from customer where c.startingBalance > 10000;")
+	List<Customer> getHighBalanceCustomers();
 }
